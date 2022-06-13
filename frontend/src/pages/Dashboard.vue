d32280c0cca362e17711517f52a945b0551094ac
Piotr Rogowski <62942330+rogowski-piotr@users.noreply.github.com>
2022-06-13 19:04:25 +0200

    Add handle of notification about new element

--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -3,9 +3,15 @@
     <Navbar/>
     <div class="row">
       <div class="col-8 mx-sm-auto p-4">
+
+        <div v-if="addStatus" class="alert alert-success text-center" role="alert" id="alert-valid-data">
+            <h4 class="alert-heading">Poprawnie dodano zdjęcie!</h4>
+        </div>
+
         <div class="col-md-12 text-center">
             <a class="btn btn-success text-black mb-4 p-3 my-2" v-bind:href="'/dodaj'">Dodaj nowe zdjęcie</a>
         </div>
+
         <TaskTable v-if="this.todos.length > 0" v-bind:todos="todos"/>
         <div v-else class="col-md-12 text-center">
           <hr>
@@ -27,9 +33,13 @@ export default {
   components: { TaskTable, Navbar },
   name: 'Dashboard',
   data () {
-    return { todos: [] }
+    return { 
+      todos: [],
+      addStatus: false,
+    }
   },
   created() {
+    this.addStatus = this.$route.query.addStatus;
     taskService.fetchTasks()
       .then(response => {
         console.log(response);
